900b14418cd7e6c3120621f4211e2c5ae93eb2f4
Alex <alexageew0506147048@gmail.com>
2025-02-27 19:13:52 +0100

    fix problem

--- js/task-2.js
+++ js/task-2.js
@@ -1,21 +1,23 @@
 class Storage {
   
+  #items; 
+
   constructor(initialItems) {
-    this.items = initialItems;
+    this.#items = initialItems;
   }
 
   getItems() {
-    return this.items;
+    return this.#items; 
   }
 
   addItem(newItem) {
-    this.items.push(newItem);
+    this.#items.push(newItem); 
   }
 
   removeItem(itemToRemove) {
-    const index = this.items.indexOf(itemToRemove);
+    const index = this.#items.indexOf(itemToRemove);
     if (index !== -1) {
-      this.items.splice(index, 1);
+      this.#items.splice(index, 1);
     }
   }
 }
