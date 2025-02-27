f86e400accc31be9afee121dae56433ffba5f123
Alex <alexageew0506147048@gmail.com>
2025-02-27 19:22:17 +0100

    added private

--- js/task-3.js
+++ js/task-3.js
@@ -1,25 +1,25 @@
 class StringBuilder {
 
-  #value; 
-
+  #value
   constructor(initialValue) {
-    this.#value = initialValue; 
+    this.#value = initialValue;
   }
 
   getValue() {
-    return this.#value; 
+    return this.#value;
   }
 
   padEnd(str) {
-    this.#value += str; 
+    this.#value += str;
   }
 
   padStart(str) {
-    this.#value = str + this.#value; 
+    this.#value = str + this.#value;
   }
 
   padBoth(str) {
     this.#value = str + this.#value + str;
+  }
 }
 
 const builder = new StringBuilder(".");
